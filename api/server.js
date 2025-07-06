abe11a0ca865d9dfc71d4217f1eb3b2f42858665
wfly <953212389@qq.com>
2025-07-06 11:25:15 +0800

    feat:!

--- api/server.js
+++ api/server.js
@@ -3,5 +3,5 @@ import { fileURLToPath } from 'node:url'
 
 export default async function handler(req, res) {
     const __dirname = path.dirname(fileURLToPath(import.meta.url))
-    res.status(200).send('88888888', __dirname)
+    res.status(200).send(`88888888${__dirname}`)
 }
