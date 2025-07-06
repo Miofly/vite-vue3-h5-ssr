acaae78c7de69fb33dbb86b985ba91db42ed5fd0
wfly <953212389@qq.com>
2025-07-06 11:37:46 +0800

    feat:!

---
+++ api/server.ts
@@ -0,0 +1,7 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+export default async function handler(req, res) {
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
+    res.status(200).send(`8cess8888888${__dirname}`)
+}
