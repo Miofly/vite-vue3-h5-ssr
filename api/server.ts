2cd035b19e98a512ea62991fd9510b7f51a2925a
wfly <953212389@qq.com>
2025-07-06 11:45:44 +0800

    feat:!

--- api/server.ts
+++ api/server.ts
@@ -4,6 +4,9 @@ import { resolve } from 'node:path'
 export default async function handler(req, res) {
     // const app = express()
     // const __dirname = path.dirname(fileURLToPath(import.meta.url))
-    const template = fs.readFileSync(resolve('client/index.html'), 'utf-8')
+    const template = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
+    console.log(template)
     res.status(200).set({ 'Content-Type': 'text/html' }).end(template)
+
+    res.status(200).send(`2${__dirname}`)
 }
