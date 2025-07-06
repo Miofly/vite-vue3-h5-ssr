d1319a94d72a4bc80190a625a68b78139ec0bb95
wfly <953212389@qq.com>
2025-07-06 12:31:32 +0800

    feat:!

--- api/server.ts
+++ api/server.ts
@@ -6,6 +6,7 @@ export default async function handler(req: any, res: any) {
         // 读取 HTML 模板文件
         const template = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
 
+        console.log(template, '===')
         // 设置正确的 Content-Type 并返回 HTML
         res.setHeader('Content-Type', 'text/html')
         res.status(200).end(template)
