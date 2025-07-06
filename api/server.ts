f106806a8c83ae385e27f86108a6350b505f3cec
wfly <953212389@qq.com>
2025-07-06 11:46:52 +0800

    feat:!

--- api/server.ts
+++ api/server.ts
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     // const __dirname = path.dirname(fileURLToPath(import.meta.url))
     const template = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
     console.log(template)
-    res.status(200).set({ 'Content-Type': 'text/html' }).end(template)
+    // res.status(200).set({ 'Content-Type': 'text/html' }).end(template)
 
-    res.status(200).send(`2${__dirname}`)
+    res.status(200).send(`2${template}`)
 }
