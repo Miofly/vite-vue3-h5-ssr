dce2b09459d6ebc290e08799c86036a43016a089
wfly <953212389@qq.com>
2025-07-06 13:04:18 +0800

    feat:!

--- api/server.ts
+++ api/server.ts
@@ -68,6 +68,8 @@ export default async function handler(req: any, res: any) {
     // 解析 cookies 中间件
     app.use(cookieParser())
 
+    res.status(200).send(`88888888${__dirname}`)
+
     app.use('/{*default}', async (req, res) => {
         try {
             // const url = req.originalUrl.replace('/test/', '/')
