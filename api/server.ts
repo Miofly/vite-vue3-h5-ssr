8ca1f8a86f2b3f669bda842e9acbb012e9f2b8af
wfly <953212389@qq.com>
2025-07-06 11:38:55 +0800

    feat:!

--- api/server.ts
+++ api/server.ts
@@ -1,7 +1,7 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-export default async function handler(req, res) {
+async function handler(req, res) {
     const __dirname = path.dirname(fileURLToPath(import.meta.url))
-    res.status(200).send(`8cess8888888${__dirname}`)
+    res.status(200).send(`2${__dirname}`)
 }
