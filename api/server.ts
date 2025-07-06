45b3a255c5360352dab7230d0564b6ea9c0e51d9
wfly <953212389@qq.com>
2025-07-06 12:34:14 +0800

    feat:!

--- api/server.ts
+++ api/server.ts
@@ -1,18 +1,97 @@
+import type { RenderType } from '@/types'
 import fs from 'node:fs'
-import { resolve } from 'node:path'
+import path from 'node:path'
+
+import { fileURLToPath } from 'node:url'
+import { UTC2Date } from '@lincy/utils'
+import compression from 'compression'
+import cookieParser from 'cookie-parser'
+import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'
+import logger from 'morgan'
+import requestIp from 'request-ip'
+import serveStatic from 'serve-static'
 
 export default async function handler(req: any, res: any) {
-    try {
-        // 读取 HTML 模板文件
-        const template = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
+    const resolve = (p: string) => path.resolve(__dirname, p)
+    const template = fs.readFileSync(resolve('client/index.html'), 'utf-8')
+    const manifest = JSON.parse(fs.readFileSync(resolve('client/.vite/ssr-manifest.json'), 'utf-8'))
+    const app = express()
 
-        console.log(template, '===')
-        // 设置正确的 Content-Type 并返回 HTML
-        res.setHeader('Content-Type', 'text/html')
-        res.status(200).end(template)
-    }
-    catch (error) {
-        console.error('Error reading template:', error)
-        res.status(500).json({ error: 'Internal Server Error' })
-    }
+    logger.token('remote-addr', (req) => {
+        return requestIp.getClientIp(req) || 'unknown'
+    })
+
+    logger.token('date', () => {
+        return UTC2Date(undefined, 'yyyy-mm-dd hh:ii:ss.SSS')
+    })
+
+    // Node.js 日志中间件
+    app.use(
+        logger('[:remote-addr] [:date] ":method :url" :status :res[content-length] ":referrer"', {
+            skip(req) {
+                const skipExt = ['.webmanifes', '.php', '.txt', '.map', '.js', '.css', '.png', 'jpg', '.jpeg', '.gif', '.ttf', '.woff2', '.ico']
+                return skipExt.some((ext) => {
+                    return req.url.includes(ext)
+                })
+            },
+        }),
+    )
+
+    // Node.js 压缩中间件
+    app.use(compression())
+    // Node.js 代理中间件, 也可以在 nginx 直接配置, 那么将不会走这里的代理中间件
+    app.use(
+        createProxyMiddleware({
+            target: 'http://php.mmxiaowu.com',
+            changeOrigin: true,
+            pathFilter: ['/api/**'],
+            pathRewrite: {
+                '^/api': '/api',
+            },
+            on: {
+                proxyReq(proxyReq, req) {
+                    proxyReq.setHeader('X-Real-IP', requestIp.getClientIp(req) || 'unknown')
+                },
+            },
+        }),
+    )
+    // Node.js 静态资源中间件
+    app.use(
+        serveStatic(resolve('client'), {
+            index: false,
+        }),
+    )
+
+    // 解析 application/json 中间件
+    app.use(express.json())
+    // 解析 application/x-www-form-urlencoded 中间件
+    app.use(express.urlencoded({ extended: true }))
+    // 解析 cookies 中间件
+    app.use(cookieParser())
+
+    app.use('/{*default}', async (req, res) => {
+        try {
+            // const url = req.originalUrl.replace('/test/', '/')
+            const url = req.originalUrl
+
+            // @ts-expect-error 未编译, 目录不对, 该文件不存在
+            const render = (await import('./server/entry-server.js')).render
+
+            const { html: appHtml, preloadLinks, headTags } = await render(url, manifest, req) as RenderType
+
+            const html = template
+                .replace('<!--preload-links-->', preloadLinks)
+                .replace('<!--app-html-->', appHtml)
+                .replace('<!--head-tags-->', headTags)
+
+            res.status(200).end(html)
+        }
+        catch (e: unknown) {
+            const err = e as Error
+            console.log(err.stack)
+            res.status(500).end(err.stack)
+        }
+    })
 }
